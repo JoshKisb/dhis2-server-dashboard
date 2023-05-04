8fb189d84a27d71be9526747da1449dd8d525ded
Josh Kisb <joshuakisb@gmail.com>
2023-05-04 09:20:27 +0300

    change api url

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,4 @@
 export const getApiUrl = (): string => {
     // return "http://217.76.51.191:8080";//process.env.API_URL as string;
-    return "http://174.138.191.155:3000"
+    return "https://ug.sk-engine.cloud/serverdash-api"
 }
